41c24c288ae375b7fc832318618f3b27ca4aa3e2
Igor, Borealis laptop <bistrovic.igor@gmail.com>
2020-02-25 15:04:59 +0100

    EF works2

---
+++ DataLibrary/Models/EntryEntities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace DataLibrary.Models
+{
+    public partial class EntryEntities : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public EntryEntities() : base("FininexDatabase") 
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+    }
+}
