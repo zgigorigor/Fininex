4d6d3eb148f971089c8bce7d3f4f6187156da6f2
Igor, PC <bistrovic.igor@gmail.com>
2020-03-05 16:20:13 +0100

    scaffold-dbcontext

---
+++ Fininex/Models/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fininex.Models
+{
+    public partial class Entry
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public string Currency { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Fininex/Models/FininexDatabaseContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Fininex.Models
+{
+    public partial class FininexDatabaseContext : DbContext
+    {
+        public FininexDatabaseContext()
+        {
+        }
+
+        public FininexDatabaseContext(DbContextOptions<FininexDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Entry> Entry { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=FininexDatabase;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Entry>(entity =>
+            {
+                entity.Property(e => e.Currency)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
